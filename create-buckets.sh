9149786b27ab542bab87907aaca8cc8a9fe95ea5
Kyle Lape <klape@redhat.com>
2025-06-05 10:15:38 -0500

    Don't run npm ci again in docs task

--- create-buckets.sh
+++ create-buckets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for bucket in "go-cache" "go-mod-cache" "npm-cache" "scanner-mod-cache"; do
+for bucket in "go-cache" "go-mod-cache" "npm-cache" "scanner-cache" "scanner-mod-cache"; do
     mc mb minio/$bucket
     for f in $(ls "buckets/$bucket"); do
         mc put "./buckets/$bucket/$f" "minio/$bucket/$f"
