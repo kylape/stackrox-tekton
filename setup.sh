b0f0fcc0f304876c78dc0ef3157a9e727f6c73ac
Kyle Lape <klape@redhat.com>
2025-06-09 14:08:22 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl patch configmap feature-flags -n tekton-pipelines --type='merge' -p='{"data":{"disable-affinity-assistant":"true"}}' # for KinD
+tkn hub install task git-clone
+tkn hub install task buildah
+kubectl create -f resources/task-fetch-cache.yaml -f resources/task-put-cache.yaml
+kubectl create -f resources/pvc-minio.yaml -f resources/deploy-minio.yaml
+kubectl expose deploy/minio --port 9000 --name minio
+kubectl create -f resources/pipeline.yaml
