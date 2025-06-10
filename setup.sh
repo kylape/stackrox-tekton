9705c472545d5c8b28ab2e85e2bea05617c1ca57
Kyle Lape <klape@redhat.com>
2025-06-10 21:38:17 +0000

    wait for tekton to be ready

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl patch configmap feature-flags -n tekton-pipelines --type='merge' -p='{"data":{"disable-affinity-assistant":"true"}}' # for KinD
+kubectl -n tekton-pipelines wait --for=condition=Available deploy/tekton-pipelines-webhook
 tkn hub install task git-clone
 tkn hub install task buildah
 kubectl create -f resources/task-fetch-cache.yaml -f resources/task-put-cache.yaml
