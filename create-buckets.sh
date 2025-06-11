167a08b13db5e4d2c3edfff32d4c6eb2a48d6307
Kyle Lape <klape@redhat.com>
2025-06-11 13:24:20 +0000

    Update set up scripts

--- create-buckets.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for bucket in "go-cache" "go-mod-cache" "npm-cache" "scanner-cache" "scanner-mod-cache"; do
-    mc mb minio/$bucket
-    for f in $(ls "buckets/$bucket"); do
-        mc put "./buckets/$bucket/$f" "minio/$bucket/$f"
-    done
-done
--- setup.sh
+++ setup.sh
@@ -2,10 +2,27 @@
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl patch configmap feature-flags -n tekton-pipelines --type='merge' -p='{"data":{"disable-affinity-assistant":"true"}}' # for KinD
+
+kubectl create -f resources/pvc-minio.yaml -f resources/deploy-minio.yaml
+kubectl expose deploy/minio --port 9000 --name minio
+kubectl -n tekton-pipelines wait --for=condition=Available deploy/minio
+
+k port-forward svc/minio 9000:9000 &
+port_forward_pid=$!
+mc alias set minio http://localhost:9000 minioadmin minioadmin
+
+for bucket in "go-cache" "go-mod-cache" "npm-cache" "scanner-cache" "scanner-mod-cache"; do
+    mc mb minio/$bucket
+    if [[ -d "buckets/$bucket" ]]; then
+        for f in $(ls "buckets/$bucket"); do
+            mc put "./buckets/$bucket/$f" "minio/$bucket/$f"
+        done
+    fi
+done
+
+kill $port_forward_pid
+
 kubectl -n tekton-pipelines wait --for=condition=Available deploy/tekton-pipelines-webhook
 tkn hub install task git-clone
 tkn hub install task buildah
-kubectl create -f resources/task-fetch-cache.yaml -f resources/task-put-cache.yaml
-kubectl create -f resources/pvc-minio.yaml -f resources/deploy-minio.yaml
-kubectl expose deploy/minio --port 9000 --name minio
-kubectl create -f resources/pipeline.yaml
+kubectl create -f resources/pipeline.yaml -f resources/cluster-role-binding.yaml -f resources/task-fetch-cache.yaml -f resources/task-put-cache.yaml
