21097324869ec1edc5237910f65e2965b22c44c1
Kyle Lape <klape@redhat.com>
2025-06-13 21:45:34 +0000

    Build the builder using tekton

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM quay.io/fedora/fedora:43
 
-COPY content/go1.24.2.linux-amd64.tar.gz /
+COPY content/go.tar.gz /
 COPY content/mc /usr/bin
 RUN chmod +x /usr/bin/mc
-RUN tar xzf /go1.24.2.linux-amd64.tar.gz 
+RUN tar xzf /go.tar.gz 
 COPY content/bundle.zip /
 RUN dnf install -y make git gcc podman zip npm nodejs jq npm nodejs zstd python3
 RUN npm install -g swagger2openapi
---
+++ download-content.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+mkdir -p content
+cd content
+
+# This assumes the bundle has alread been placed here by hand for now at least
+mc alias set minio http://minio:9000 minioadmin minioadmin
+mc get minio/bundle/bundle.zip .
+
+if [ $(uname -m) = x86_64 ]; then
+    curl https://dl.min.io/client/mc/release/linux-amd64/mc > mc
+    curl https://go.dev/dl/go1.24.4.linux-amd64.tar.gz > go.tar.gz
+fi
+
+if [ $(uname -m) = aarch64 ]; then 
+    curl https://dl.min.io/client/mc/release/linux-arm64/mc > mc
+    curl https://go.dev/dl/go1.24.4.linux-arm64.tar.gz > go.tar.gz
+fi
+
+chmod +x mc
