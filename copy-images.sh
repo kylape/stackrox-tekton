834a606e39b8c6db2bcade89564240d507b70ef2
Kyle Lape <klape@redhat.com>
2025-06-22 20:32:33 +0000

    Copy main stackrox image from quay

--- copy-images.sh
+++ copy-images.sh
@@ -8,3 +8,4 @@ for image in "scanner-db" "scanner-v4-db" "central-db"; do
     skopeo copy docker://registry.redhat.io/advanced-cluster-security/rhacs-$image-rhel8:$TAG docker://kind-registry:5000/stackrox/$image:latest --dest-tls-verify=false
 done
 
+skopeo copy docker://quay.io/klape/stackrox:latest docker://kind-registry:5000/stackrox/stackrox:latest --dest-tls-verify=false
