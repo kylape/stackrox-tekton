33cbfdee0b7e1affbe53b3bdfa345cef18cb8a18
Kyle Lape <klape@redhat.com>
2025-07-18 22:28:38 -0500

    Clean up setup script

--- setup.sh
+++ setup.sh
@@ -11,24 +11,5 @@ kubectl expose deploy/minio --port 9000 --name minio
 sleep 1
 kubectl wait --for=condition=Available deploy/minio
 
-kubectl port-forward svc/minio 9000:9000 &
-port_forward_pid=$!
-sleep 1
-mc alias set minio http://localhost:9000 minioadmin minioadmin
-
-for bucket in "go-cache" "go-mod-cache" "npm-cache" "scanner-cache" "scanner-mod-cache"; do
-    mc mb minio/$bucket
-    if [[ -d "buckets/$bucket" ]]; then
-        for f in $(ls "buckets/$bucket"); do
-            mc put "./buckets/$bucket/$f" "minio/$bucket/$f"
-        done
-    fi
-done
-
-kill $port_forward_pid
-
-kubectl -n tekton-pipelines wait --for=condition=Available deploy/tekton-pipelines-webhook
-tkn hub install task git-clone
-tkn hub install task buildah
-kubectl create sa admin || true
-kubectl create -f resources/stackrox
+mc alias set minio http://minio.default.svc:9000 minioadmin minioadmin
+mc mb minio/cache
